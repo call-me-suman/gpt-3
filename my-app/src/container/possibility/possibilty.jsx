e4d9c02143f23b0a83bfc9ac7843347b851cf2e9
call-me-suman <114657729+call-me-suman@users.noreply.github.com>
2023-12-21 22:19:08 +0530

    Add files via upload

---
+++ my-app/src/container/possibility/possibilty.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Possibilty = () => {
+  return (
+    <div>possibilty</div>
+  )
+}
+
+export default Possibilty
---
+++ my-app/src/container/whatgpt3/whatgpt3.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Whatgpt3 = () => {
+  return (
+    <div>whatgpt3</div>
+  )
+}
+
+export default Whatgpt3
